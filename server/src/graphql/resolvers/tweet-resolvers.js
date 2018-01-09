15c8acd5651ec697d03cbc09e07dcf5784a3145c
Dan Constantinescu <dan_constantinescu@outlook.com>
2018-01-09 21:32:27 +0200

    be able to receive only one tweet

--- server/src/graphql/resolvers/tweet-resolvers.js
+++ server/src/graphql/resolvers/tweet-resolvers.js
@@ -1,5 +1,6 @@
 import Tweet from '../../models/Tweet';
 
 export default {
-    getTweets: () => Tweet.find({})
+    getTweet: (_, { _id }) => Tweet.findById(_id),
+    getTweets: () => Tweet.find({}),
 }
--- server/src/graphql/schema.js
+++ server/src/graphql/schema.js
@@ -1,10 +1,11 @@
 export default`
     type Tweet {
-        _id: String
-        text: String
+        _id: ID!
+        text: String!
     }
 
     type Query {
+        getTweet(_id: ID!): Tweet
         getTweets: [Tweet]
     }
 
