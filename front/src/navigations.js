7f561ea472e7262bd28bbf85a0ad5c8b794a4604
Dan Constantinescu <dan_constantinescu@outlook.com>
2018-01-29 19:41:58 +0200

    added react-navigation connected to redux

---
+++ front/src/navigations.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { addNavigationHelpers, StackNavigator, TabNavigator } from 'react-navigation';
+import { connect } from 'react-redux';
+
+import HomeScreen from './screens/HomeScreen';
+
+const AppMainNav = StackNavigator({
+    Home: {
+        screen: HomeScreen
+    }
+})
+
+class AppNavigator extends Component {
+    render() {
+        const nav = addNavigationHelpers({
+            dispatch: this.props.dispatch,
+            state: this.props.nav
+        });
+        return (
+            <AppMainNav navigation={nav} />
+        );
+    }
+}
+
+export default connect(state => ({
+    nav: state.nav
+}))(AppNavigator);
+
+export const router = AppMainNav.router;
--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
 
+import nav from './navigations';
+
 export default client => combineReducers({
     apollo: client.reducer(),
+    nav
 });
